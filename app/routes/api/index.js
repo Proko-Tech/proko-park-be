87ddda463ebe6213684ba5d12cb11eed6c7f095d
Thomas Li <thomasligreat11@gmail.com>
2020-12-30 15:07:08 -0800

    fix: changed file layout

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -6,8 +6,13 @@ const verifyParkingLotToken = require('../../middlewares/parking_lot/verifyParki
 const parkingLotRouter = require('./parking_lot');
 const parkingLotAuthenticateRouter = require('./parking_lot_authenticate');
 
+const userRoute = require('./users');
+
 // routes
 router.use('/parking_lot', verifyParkingLotToken, parkingLotRouter);
 router.use('/parking_lot_authenticate', parkingLotAuthenticateRouter);
+
+// user routes:
+router.use('/user', userRoute);
 
 module.exports = router;
