542687013032d1ee3ca0dfdcbc5b6c929857021f
YuyusanK <39747989+YuyusanK@users.noreply.github.com>
2020-12-30 16:53:24 -0800

    Update 20201230111820_users_table.js

--- database/migrations/20201230111820_users_table.js
+++ database/migrations/20201230111820_users_table.js
@@ -2,10 +2,11 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', (tbl) => {
         tbl.increments('id').unique().notNullable();
+        tbl.text('first_name').notNullable();
+        tbl.text('last_name').notNullable();
         tbl.text('email').notNullable();
         tbl.text('phone_number').notNullable();
-        tbl.text('password').notNullable()
-        tbl.text('secret').notNullable();
+        tbl.text('password').notNullable();
         tbl.enu('sign_up_type', ['NATIVE', 'GOOGLE', 'FACEBOOK', 'APPLE']);
     });
 };
