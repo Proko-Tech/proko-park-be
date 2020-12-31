c9e04247cfcd673a1e03a6f4b2dbb46f6d8c0eaf
YuyusanK <39747989+YuyusanK@users.noreply.github.com>
2020-12-30 16:57:04 -0800

    Update 20201230111820_users_table.js

--- database/migrations/20201230111820_users_table.js
+++ database/migrations/20201230111820_users_table.js
@@ -1,4 +1,9 @@
-
+const sign_up_type_enum = [
+    'NATIVE',
+    'GOOGLE',
+    'FACEBOOK', 
+    'APPLE',
+];
 exports.up = function(knex) {
     return knex.schema.createTable('users', (tbl) => {
         tbl.increments('id').unique().notNullable();
@@ -7,7 +12,8 @@ exports.up = function(knex) {
         tbl.text('email').notNullable();
         tbl.text('phone_number').notNullable();
         tbl.text('password').notNullable();
-        tbl.enu('sign_up_type', ['NATIVE', 'GOOGLE', 'FACEBOOK', 'APPLE']);
+        tbl.enum('sign_up_type', ['NATIVE', 'GOOGLE', 'FACEBOOK', 'APPLE']);
+        tbl.enum('sign_up_type', sign_up_type_enum, {useNative: true, enumName:'sign_up_type_enum'}).notNullable().index();
     });
 };
 
