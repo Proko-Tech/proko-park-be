7e3a94860ac52ff9f590ab130d94490e20ab2ad4
Thomas Li <thomasligreat11@gmail.com>
2021-01-14 20:44:28 -0800

    feat: added /user/parking_lot

--- database/models/reservationModel.js
+++ database/models/reservationModel.js
@@ -1,16 +1,25 @@
 const db = require('../dbConfig');
+const spotModel = require('./spotsModel');
 
 /**
  * get distinct lot information from user parking histories
  * @param user_id
- * @returns {Promise<void>}
+ * @returns {Promise<unknown[]>}
  */
 async function getDistinctLotsByUserId(user_id){
-    const result = await db('reservations')
+    const rows = await db('reservations')
         .join('lots', 'reservations.lot_id', 'lots.id')
         .where({user_id})
         .distinct(['lot_id', 'name', 'lat', 'long', 'address', 'state', 'city', 'zip']);
+    const result = await Promise.all(rows.map(async (row)=>{
+        const spots = await spotModel.getUnoccupiedByLotId(row.lot_id);
+        const lot_info = {
+            ...row,
+            available_spots: spots.length,
+        };
+        return lot_info;
+    }));
     return result;
 }
 
-module.exports={getDistinctLotsByUserId}
+module.exports={getDistinctLotsByUserId};
