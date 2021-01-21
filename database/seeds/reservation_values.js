3446e0d7c03056d1bff5caac682c8453d353529e
Thomas Li <thomasligreat11@gmail.com>
2021-01-20 16:45:31 -0800

    feat: added alive status

--- database/seeds/reservation_values.js
+++ database/seeds/reservation_values.js
@@ -7,6 +7,7 @@ exports.seed = function(knex) {
             return knex('reservations').insert([
                 {id: 1, user_id: 1, vehicle_id: 1, spot_id:1, lot_id:1, elapsed_time:2.5, total_price: 30.00, is_paid:1},
                 {id: 2, user_id: 1, vehicle_id: 1, spot_id:3, lot_id:1, elapsed_time:1.2, total_price: 10.00, is_paid:1},
+                {id: 3, user_id: 1, vehicle_id: 1, spot_id:7, lot_id:2, elapsed_time:1.2, total_price: 10.00, is_paid:1},
             ]);
         });
 };
