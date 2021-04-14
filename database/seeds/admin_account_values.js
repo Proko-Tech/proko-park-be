71e94c1d125cdf3925e3553d3cb6cef445ecbbec
YuyusanK <39747989+YuyusanK@users.noreply.github.com>
2021-04-13 22:16:37 -0700

    Update admin_account_values.js

--- database/seeds/admin_account_values.js
+++ database/seeds/admin_account_values.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
     return knex('table_name').del()
         .then(function() {
             // Inserts seed entries
-            return knex('table_name').insert([
+            return knex('admin_accounts').insert([
                 {id: 1, admin_fname: 'Yuichi', admin_lname:'Kurino', admin_email: 'kurinoyuichi@gmail.com', admin_phone_number: '12345678910', admin_username: 'ykurino', organization_name: 'YK org', membership_status:'ACTIVE', notes: 'yuichik notes'},
                 {id: 2, admin_fname: 'Jessica', admin_lname:'Hilario', admin_email: 'jessicarhilario@gmail.com', admin_phone_number: '12345678910', admin_username: 'jhilario', organization_name: 'JH org', membership_status:'ACTIVE', notes: 'jessicah notes'},
             ]);
