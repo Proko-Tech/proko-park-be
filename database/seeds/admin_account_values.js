bf2fc7ba6c7277bcadad4caaafd2c177c9b427fd
Thomas Li <thomasligreat11@gmail.com>
2021-05-04 11:02:20 -0700

    fix: increaased range of parking lot lat long

--- database/seeds/admin_account_values.js
+++ database/seeds/admin_account_values.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
         .then(function() {
             // Inserts seed entries
             return knex('admin_accounts').insert([
-                {id: 1, admin_email: 'kurinoyuichi@gmail.com', admin_phone_number: '12345678910', admin_username: 'ykurino', organization_name: 'YK org', membership_status:'ACTIVE', notes: 'yuichik notes'},
-                {id: 2, admin_email: 'jessicarhilario@gmail.com', admin_phone_number: '12345678910', admin_username: 'jhilario', organization_name: 'JH org', membership_status:'ACTIVE', notes: 'jessicah notes'},
+                {id: 1, admin_email: 'kurinoyuichi@gmail.com', admin_phone_number: '12345678910', admin_username: 'ykurino', admin_password_hash:'$2a$10$fBRo3pF3BVUpAru0bKxwQ.OQ9hn3CBWqTmpyKTJEJZ32/uBeKp5x6', organization_name: 'YK org', membership_status:'ACTIVE', notes: 'yuichik notes'},
+                {id: 2, admin_email: 'jessicarhilario@gmail.com', admin_phone_number: '12345678910', admin_username: 'jhilario', admin_password_hash:'$2a$10$fBRo3pF3BVUpAru0bKxwQ.OQ9hn3CBWqTmpyKTJEJZ32/uBeKp5x6', organization_name: 'JH org', membership_status:'ACTIVE', notes: 'jessicah notes'},
             ]);
         });
 };
