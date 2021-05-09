97a0f324b6113a3cd92561e449c7544a710e317e
YuyusanK <kurinoyuichi@gmail.com>
2021-05-08 20:46:23 -0700

    fix: changed table name/added time stamps

--- database/migrations/20210508191422_add_admin_parking_relation.js
+++ database/migrations/20210508191422_add_admin_parking_relation.js
@@ -1,12 +1,13 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('admin_lot_relations', (tbl) => {
+    return knex.schema.createTable('lot_ownerships', (tbl) => {
         tbl.increments('id').unique().notNullable();
         tbl.integer('admin_id').notNullable();
         tbl.integer('lot_id').notNullable();
+        tbl.timestamps(true, true);
     });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('admin_lot_relations');
+    return knex.schema.dropTable('lot_ownerships');
 };
 
