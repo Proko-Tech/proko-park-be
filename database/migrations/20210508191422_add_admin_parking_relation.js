e13e53a92eaa01dd685610c36b60ee5c62d9849e
YuyusanK <kurinoyuichi@gmail.com>
2021-05-08 19:59:57 -0700

    fix: create a new relations table instead of adding a coloumn in lots

--- database/migrations/20210508191422_add_admin_parking_relation.js
+++ database/migrations/20210508191422_add_admin_parking_relation.js
@@ -1,12 +1,12 @@
-
 exports.up = function(knex) {
-    return knex.schema.alterTable('lots', (tbl) => {
+    return knex.schema.createTable('admin_lot_relations', (tbl) => {
+        tbl.increments('id').unique().notNullable();
         tbl.integer('admin_id').notNullable();
+        tbl.integer('lot_id').notNullable();
     });
 };
 
 exports.down = function(knex) {
-    return knex.schema.alterTable('lots', (tbl) => {
-        tbl.integer('admin_id').notNullable();
-    });
+    return knex.schema.dropTable('admin_lot_relations');
 };
+
