3e4e9f65a62eb00b304b4cd9d5c1ccd7b9750e1d
Thomas Li <thomasligreat11@gmail.com>
2021-05-31 21:26:45 -0700

    removed index route time

--- app/routes/index/index.js
+++ app/routes/index/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const {DateTime} = require('luxon');
-
-
 router.get('/', async function(req, res){
     res.status(200).json({message: 'Proko Park Server alive'});
 });
