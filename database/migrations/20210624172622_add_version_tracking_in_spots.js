1fc6d100d302adbb0b6386a0d2b360b6977a02d9
Thomas Li <thomasligreat11@gmail.com>
2021-06-24 17:46:35 -0700

    fix: added default to now

--- database/migrations/20210624172622_add_version_tracking_in_spots.js
+++ database/migrations/20210624172622_add_version_tracking_in_spots.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.table('spots', (tbl) => {
         tbl.integer('firmware_version').defaultTo(1);
         tbl.integer('available_firmware_version').defaultTo(1);
-        tbl.dateTime('firmware_updated_at');
+        tbl.dateTime('firmware_updated_at').defaultTo(knex.fn.now());
     });
 };
 
