65312e8701983caef1beb605b8e621464dd013a9
Thomas Li <thomasligreat11@gmail.com>
2022-02-07 22:44:13 -0800

    fix: changed history to display reverse order

--- database/models/reservationModel.js
+++ database/models/reservationModel.js
@@ -39,6 +39,7 @@ async function getWithLotByUserId(user_id){
     const rows = await db('reservations')
         .join('lots','reservations.lot_id', 'lots.id')
         .where({user_id})
+        .orderBy('reserved_at', 'desc')
         .select('*');
     return rows;
 }
