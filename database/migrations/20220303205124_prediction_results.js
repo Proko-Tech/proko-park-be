ef2894d3ce9249d45960a877c72daddde16be2a3
YuyusanK <kurinoyuichi@gmail.com>
2022-03-03 21:39:18 -0800

    fix: changed table header to be more readable

--- database/migrations/20220303205124_prediction_results.js
+++ database/migrations/20220303205124_prediction_results.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
     return knex.schema.createTable('prediction_results', (tbl)=>{
         tbl.increments('id').unique().notNullable();
         tbl.text('image_url').notNullable();
-        tbl.boolean('prediction_is_car').notNullable();
+        tbl.boolean('is_car_prediction').notNullable();
         tbl.boolean('true_car_prediction');
-        tbl.boolean('prediction_license_plate').notNullable();
+        tbl.boolean('license_plate_prediction').notNullable();
         tbl.boolean('true_license_prediction');
         tbl.timestamp('created_at').notNullable();
         tbl.text('spot_secret').notNullable();
