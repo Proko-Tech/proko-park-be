bf602741f7fe313a6e547421af28c1d7fdc0569b
Thomas Li <thomasligreat11@gmail.com>
2022-03-13 18:24:41 -0700

    migrate test to jest

---
+++ test/routes/auth.test.js
@@ -0,0 +1,95 @@
+const app = require('../../app/index');
+const supertest = require('supertest');
+
+const db = require('../../database/dbConfig');
+jest.mock('../../database/models/usersModel');
+const usersModel = require('../../database/models/usersModel');
+
+describe('authentication endpoints', () => {
+    describe('POST /api/user_authenticate', () => {
+        it('should return status 202 when the user is found', async () => {
+            usersModel.getByEmail = (email) => {
+                return [
+                    {
+                        id:1, first_name: 'test', last_name: 'test', email,
+                        password: '$2a$10$JhHw7tbG9bDdMKoSqjGa0.HoKSWKTZM37kShbE0VjfCPo0bGU4Phu', sign_up_type: 'NATIVE',
+                    },
+                ];
+            }
+
+            const res = await supertest(app).post('/api/user_authenticate/').send({userData: {email:'test@email.com', password: '123', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(202);
+        });
+
+        it('should return status 404 when password not match', async () => {
+            usersModel.getByEmail = (email) => {
+                return [
+                    {
+                        email,
+                        password: '$2a$10$JhHw7tbG9bDdMKoSqjGa0.HoKSWKTZM37kShbE0VjfCPo0bGU4Phu', sign_up_type: 'NATIVE',
+                    },
+                ];
+            }
+
+            const res = await supertest(app).post('/api/user_authenticate/').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(404);
+        });
+
+        it('should return status 404 when email not found', async () => {
+            usersModel.getByEmail = () => {
+                return [];
+            }
+
+            const res = await supertest(app).post('/api/user_authenticate/').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(404);
+        });
+
+        it('should return status 401 when login type not match', async () => {
+            usersModel.getByEmail = (email) => {
+                return [
+                    {
+                        email,
+                        password: '$2a$10$JhHw7tbG9bDdMKoSqjGa0.HoKSWKTZM37kShbE0VjfCPo0bGU4Phu', sign_up_type: 'FACEBOOK',
+                    },
+                ];
+            }
+
+            const res = await supertest(app).post('/api/user_authenticate/').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(401);
+        });
+
+        it('should return status 401 when login type not match', async () => {
+            usersModel.getByEmail = (email) => {
+                return [
+                    {
+                        email,
+                        password: '$2a$10$JhHw7tbG9bDdMKoSqjGa0.HoKSWKTZM37kShbE0VjfCPo0bGU4Phu', sign_up_type: 'GOOGLE',
+                    },
+                ];
+            }
+
+            const res = await supertest(app).post('/api/user_authenticate/').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(401);
+        });
+    });
+
+    describe('/api/user_authenticate/social', () => {
+        it('should return status 401 when login type is native', async () => {
+            const res = await supertest(app).post('/api/user_authenticate/social').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'NATIVE'}});
+            expect(res.status).toBe(401);
+        });
+
+        it('should return status 202 when user info matched', async () => {
+            usersModel.getByEmail = (email) => {
+                return [
+                    {
+                        email,
+                        password: '$2a$10$JhHw7tbG9bDdMKoSqjGa0.HoKSWKTZM37kShbE0VjfCPo0bGU4Phu', sign_up_type: 'GOOGLE',
+                    },
+                ];
+            }
+            const res = await supertest(app).post('/api/user_authenticate/social').send({userData: {email:'test@email.com', password: 'fasdfsa', login_in_type:'GOOGLE'}});
+            expect(res.status).toBe(202);
+        });
+    });
+});
