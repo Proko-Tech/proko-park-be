312fdbc34cdec0e8f8c3cdfee91e28db402dbdc7
Kewen <k4zhao@ucsd.edu>
2022-07-11 13:05:15 -0700

    field name lowercase

--- database/migrations/20220702225402_firmware_version_table.js
+++ database/migrations/20220702225402_firmware_version_table.js
@@ -3,10 +3,10 @@ exports.up = function(knex) {
     return knex.schema.createTable('firmware_versions', (tbl)=>{
         tbl.increments('id').unique().notNullable();
         tbl.string('version').unique().notNullable();
-        tbl.text('ESP8266_url').notNullable();
-        tbl.text('ESP32_url').notNullable();
-        tbl.text('ESP8266_file_name').notNullable();
-        tbl.text('ESP32_file_name').notNullable();
+        tbl.text('esp8266_url').notNullable();
+        tbl.text('esp32_url').notNullable();
+        tbl.text('esp8266_file_name').notNullable();
+        tbl.text('esp32_file_name').notNullable();
         tbl.timestamps(true,true);// creates created_at column and updated_at column
     });
 };
