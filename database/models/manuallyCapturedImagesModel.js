95faa0922b5c1e99ae24b8865b178ea9daca5784
Ben Lau <benjilewiscn@gmail.com>
2022-08-12 04:06:22 -0700

    Update manuallyCapturedImagesModel.js

--- database/models/manuallyCapturedImagesModel.js
+++ database/models/manuallyCapturedImagesModel.js
@@ -1,18 +1,22 @@
 const db = require('../dbConfig');
-const pick = require('../../utils/pick');
-const removeDuplicates = require('../../utils/removeDuplicates');
-const {DateTime} = require('luxon');
 
 /**
- * get manual capture info by id
- * @param id
+ * get manual capture info by capture_id
+ * @param capture_id
  * @returns {Promise<void>}
  */
- async function getById(id) {
-    const rows = await db('manually_captured_images').where({id}).select('*');
-    return rows;
- }
+async function getById(capture_id) {
+	const rows = await db('manually_captured_images').where('capture_id', capture_id).select('*');
+	return rows;
+}
 
- async function getBySpotId(spot_id) {
-    const rows = await db('manually_captured_images').where('spot_id', spot_id).select('*');
- }
+
+/**
+ * get manual capture info by capture_id
+ * @param capture_id
+ * @returns {Promise<void>}
+ */
+async function getBySpotId(spot_id) {
+	const rows = await db('manually_captured_images').where('spot_id', spot_id).select('*');
+	return rows;
+}
