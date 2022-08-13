cc8fe973065c63de6fa94e6f42aa9f3cedca8add
Ben Lau <benjilewiscn@gmail.com>
2022-08-13 03:09:24 -0700

    Update 20220808163336_create_manually_captured_images_table.js

--- database/migrations/20220808163336_create_manually_captured_images_table.js
+++ database/migrations/20220808163336_create_manually_captured_images_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('manually_captured_images', (tbl) => {
         tbl.increments('capture_id').unique().notNullable();
-        tbl.integer('spot_id').notNullable();
+        tbl.text('spot_secret').notNullable();
         tbl.integer('admin_id');
         tbl.timestamp('created_at').defaultTo(knex.fn.now());
         tbl.timestamp('updated_at').defaultTo(knex.fn.now());
