199824754323caf9b95fddff451d3c4291f6b34f
Ben Lau <benjilewiscn@gmail.com>
2022-08-13 22:41:38 -0700

    Update manuallyCapturedImagesModel.js

--- database/models/manuallyCapturedImagesModel.js
+++ database/models/manuallyCapturedImagesModel.js
@@ -2,7 +2,7 @@ const db = require('../dbConfig');
 
 /**
  * get manual capture info by capture_id
- * @param capture_id
+ * @param capture_id each manual capture entry has a unique capture_id
  * @returns {Promise<void>}
  */
 async function getById(capture_id) {
@@ -12,11 +12,11 @@ async function getById(capture_id) {
 
 
 /**
- * get manual capture info by capture_id
- * @param capture_id
+ * get all manual capture info by spot_secret
+ * @param spot_secret
  * @returns {Promise<void>}
  */
-async function getBySpotId(spot_id) {
-	const rows = await db('manually_captured_images').where('spot_id', spot_id).select('*');
+async function getAllBySpotId(spot_secret) {
+	const rows = await db('manually_captured_images').where('spot_secret', spot_secret).select('*');
 	return rows;
 }
