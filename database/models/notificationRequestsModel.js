5241d02bc103bbb0eedab6d1c80954a5e1400657
Thomas <thomasligreat11@gmail.com>
2022-09-13 18:53:52 -0700

    added notification_requests_map to result

---
+++ database/models/notificationRequestsModel.js
@@ -0,0 +1,19 @@
+const db = require('../dbConfig');
+
+/**
+ * get requested and error notification requests by [user_id]
+ * @param user_id
+ * @returns {Promise<awaited Knex.QueryBuilder<TRecord, ArrayIfAlready<TResult, DeferredKeySelection<TRecord, string>>>>}
+ */
+async function getRequestedAndErrorByUserId(user_id) {
+    const rows = await db('notification_requests')
+        .where({user_id})
+        .andWhere(function() {
+            this.where('status', 'REQUESTED')
+                .orWhere('status', 'ERROR')
+        })
+        .select('*');
+    return rows;
+}
+
+module.exports={getRequestedAndErrorByUserId}
