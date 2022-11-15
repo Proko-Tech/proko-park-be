35d85e41e3e54cf34fdaef0e50c5e0bf871b5baf
Kewen Zhao <hage.zkw@gmail.com>
2022-11-15 12:56:26 -0800

    fix style

--- database/migrations/20221114214516_add_OCCUPIED_BUT_NO_CARD_enum_spot_status.js
+++ database/migrations/20221114214516_add_OCCUPIED_BUT_NO_CARD_enum_spot_status.js
@@ -5,7 +5,7 @@ const spot_status_enum = [
     'OCCUPIED',
     'OFF_LINE',
     'VIOLATION',
-    'OCCUPIED_BUT_NO_CARD'
+    'OCCUPIED_BUT_NO_CARD',
 ];
 const spot_status_enum_old = [
     'UNOCCUPIED',
@@ -18,37 +18,41 @@ const tableName = 'spots';
 const columnName = 'spot_status';
 
 exports.up = function(knex) {
-  let existRows;
-  return knex.select()
-  .from(tableName)
-  .then((rows) => {
-    existRows = rows
-    return knex.schema.table(tableName, (table) => table.dropColumn(columnName))
-  })
-  .then(() => knex.schema.table(tableName, (table) => table.enu(columnName, spot_status_enum).notNullable()))
-  .then(() => {
-    return Promise.all(existRows.map((row) => {
-      return knex(tableName)
-      .update({ spot_status: row.spot_status })
-      .where('id', row.id)
-    }))
-  })
+    let existRows;
+    return knex.select()
+        .from(tableName)
+        .then((rows) => {
+            existRows = rows
+            return knex.schema.table(tableName, (table) =>
+                table.dropColumn(columnName))
+        })
+        .then(() => knex.schema.table(tableName, (table) =>
+            table.enu(columnName, spot_status_enum).notNullable()))
+        .then(() => {
+            return Promise.all(existRows.map((row) => {
+                return knex(tableName)
+                    .update({spot_status: row.spot_status})
+                    .where('id', row.id)
+            }))
+        })
 }
 
 exports.down = function(knex) {
-  let existRows;
-  return knex.select()
-  .from(tableName)
-  .then((rows) => {
-    existRows = rows
-    return knex.schema.table(tableName, (table) => table.dropColumn(columnName))
-  })
-  .then(() => knex.schema.table(tableName, (table) => table.enu(columnName, spot_status_enum_old).notNullable()))
-  .then(() => {
-    return Promise.all(existRows.map((row) => {
-      return knex(tableName)
-      .update({ spot_status: row.spot_status === 'OCCUPIED_BUT_NO_CARD' ? 'OCCUPIED' : row.spot_status })
-      .where('id', row.id)
-    }))
-  })
+    let existRows;
+    return knex.select()
+        .from(tableName)
+        .then((rows) => {
+            existRows = rows
+            return knex.schema.table(tableName, (table) =>
+                table.dropColumn(columnName))
+        })
+        .then(() => knex.schema.table(tableName, (table) =>
+            table.enu(columnName, spot_status_enum_old).notNullable()))
+        .then(() => {
+            return Promise.all(existRows.map((row) => {
+                return knex(tableName)
+                    .update({spot_status: row.spot_status === 'OCCUPIED_BUT_NO_CARD' ? 'OCCUPIED' : row.spot_status})
+                    .where('id', row.id)
+            }))
+        })
 }
