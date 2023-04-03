2f93a9305861e89f8c837cdca6b4d12bcaecd553
Kewen Zhao <hage.zkw@gmail.com>
2023-04-03 16:41:39 -0700

    enter->entry

--- database/migrations/20230403164029_add_entry_coordinates_to_reservations.js
+++ database/migrations/20230403164029_add_entry_coordinates_to_reservations.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
     return knex.schema.table('reservations', (tbl) => {
-        tbl.text('enter_coordinates');
+        tbl.text('entry_coordinates');
     });
 };
 
 exports.down = function(knex) {
     return knex.schema.table('reservations', (tbl) => {
-        tbl.dropColumn('enter_coordinates');
+        tbl.dropColumn('entry_coordinates');
     });
 };
