44fe257514faedfdfabfb8937519409e55347a8d
Thomas Li <thomasligreat11@gmail.com>
2024-01-07 16:19:58 -0800

    changed secre type to string instead of boolean

--- database/migrations/20240107140509_add_enter_camera_and_exit_camera_to_reservations.js
+++ database/migrations/20240107140509_add_enter_camera_and_exit_camera_to_reservations.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.table('reservations', (tbl) => {
-        tbl.boolean('entry_camera_secret');
-        tbl.boolean('exit_camera_secret');
+        tbl.text('entry_camera_secret');
+        tbl.text('exit_camera_secret');
     });
 };
 
@@ -9,6 +9,5 @@ exports.down = function(knex) {
     return knex.schema.table('reservations', (tbl) => {
         tbl.dropColumn('entry_camera_secret');
         tbl.dropColumn('exit_camera_secret');
-
     });
 };
